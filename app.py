ae328a8e1dda34c6ff4b34182af679dc2000aa08
Vladimir Ignatev <ya.na.poche@gmail.com>
2015-04-07 11:36:23 +0400

    proper callback URL

--- app.py
+++ app.py
@@ -2,26 +2,31 @@
 import os
 import flask
 
+from flask import url_for
+
 from heroku import HerokuRequest
 
 app = flask.Flask(__name__)
 
 app.secret_key = 'Your secret key.'
+debug_mode = os.environ.get('DEBUG', False) == '1'
+http_prefix = 'https://'
+server_name = os.environ['SERVER_NAME']
 
-app.debug = os.environ.get('DEBUG', False) == '1'
+app.debug = debug_mode
 
 if app.debug:
     pass
 else:
-    app.config.update(SERVER_NAME=os.environ['SERVER_NAME'])
+    app.config.update(SERVER_NAME=server_name)
     app.request_class = HerokuRequest
 
-app.config.update(HTTP_PREFIX='http://')
+app.config.update(HTTP_PREFIX=http_prefix)
 
 
 @app.route('/')
 def hello_world():
-	callback = ''
+	callback = url_for('.postback')
 	service = 'UDID registration test app'
 	
 	api_link = 'https://get.udid.io/thirdparty/api/?callback=%(callback)s&service=%(service)s&schemeurl=0' % {
